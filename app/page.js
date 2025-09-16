29d99baa24ee3d6427b60f35f81be498784d1a6d
samiulalim-dev <samiulalim.dev@gmail.com>
2025-09-16 21:52:52 +0600

    added feedbackForm in landing page

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
 import Banner from "@/components/Banner";
-import Navbar from "@/components/navbar/Navbar";
 import WhyChose from "@/components/WhyChose";
 import Image from "next/image";
 import FAQ from "../components/FAQ/FAQ";
+import FeedbackForm from "../components/FeedbackForm/FeedbackForm";
 
 export default function Home() {
   return (
     <>
-     <Navbar></Navbar>
       <Banner></Banner>
       <WhyChose></WhyChose>
       <FAQ/>
+      <FeedbackForm></FeedbackForm>
     </>
   );
 }
