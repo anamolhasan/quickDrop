689a367773a9a69187db7f9d5aa42a4372d447aa
sufihasan <hasan6nh@gmail.com>
2025-09-18 11:25:32 +0600

    done by hasan

--- components/home/State/index.jsx
+++ components/home/State/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const index = () => {
     return (
         <div className='h-100'>
-           <h1 className='text-5xl text-center'>State</h1> 
+            <h1 className='text-5xl text-center'>State</h1>
         </div>
     );
 };
