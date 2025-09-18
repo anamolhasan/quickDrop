0a69794f269aaf1cc3219e13ab1e2e504baafca2
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-18 13:04:46 +0600

    contact form added

--- app/(public)/contact/page.jsx
+++ app/(public)/contact/page.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import ContactBanner from './components/ContactBanner';
+import ContactUs from './components/ContactUs';
 
 const contactPage = () => {
     return (
         <div className=' w-11/12 mx-auto '>
             {/* <h1 >contact</h1> */}
             <ContactBanner />
+            <ContactUs />
         </div>
     );
 };
