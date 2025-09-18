6e5fac5119555fdbcaaeadb93b83b7c5c1e301e7
sufihasan <hasan6nh@gmail.com>
2025-09-18 16:44:17 +0600

    add customer satisfication section by hasan at 4:44 pm

--- components/home/Review/index.jsx
+++ components/home/Review/index.jsx
@@ -1,10 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
 
 const index = () => {
     return (
-        <div className='h-100'>
+        <div className='w-11/12 mx-auto'>
             <h1 className='text-5xl text-center'>review</h1>
-        </div>
+            <div className='flex flex-col-reverse md:flex-row md:items-center
+             md:gap-16 mt-5'>
+                <div className='md:w-1/2 mt-3 md:mt-0'>
+                    <h1 className='text-3xl font-semibold'>Customer Satisfaction is Our First
+                        Priority</h1>
+                    <p className='text-justify mt-2 text-xl'>We offer the lowest delivery
+                        charge with the highest value along with 100% safety of your product.
+                        QuickDrop delivers your parcels to every corner of Bangladesh right
+                        on time.</p>
+                </div>
+                <div className='w-1/2 flex justify-end'>
+                    <Image className='rounded' width={500} height={500} src='/parcelhomedelivery.png' alt='customer setisfy img' />
+                </div>
+            </div>
+        </div >
     );
 };
 
