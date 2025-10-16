da9a3ee29c3d56e28d11feb035ea27dcd32f271c
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-16 16:37:36 +0600

    payment history data show

--- app/(admin)/dashboard/payment-history/page.jsx
+++ app/(admin)/dashboard/payment-history/page.jsx
@@ -1,11 +1,49 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 
-const page = () => {
+
+const formatDate = (iso) => new Date(iso).toLocaleString()
+
+const PaymentHistory = () => {
+    const {data: session} = useSession()
+    console.log(session)
+    console.log(session?.user?.email)
+
+    const {isPending, data : payment = []} = useQuery({
+        queryKey:['payment', session?.user?.email],
+        queryFn: async () => {
+            const res = await axios.get(`/payments?email=${session?.user?.email}`)
+            return res.data
+        }
+    })
+
+    if(isPending){
+        return <p>Loading...</p>
+    }
+    
     return (
-        <div>
-            <h1>show all payment history</h1>
+        <div className='overflow-x-auto shadow-md rounded-xl'>
+            <h1 className='text-xl md:text-2xl lg:text-4xl text-gray-400 font-semibold'>Payment history</h1>
+
+            <table className='table table-zebra w-full mt-10'>
+                <thead className='bg-amber-600 text-base font-semibold'>
+                    <tr>
+                        <th>#</th>
+                        <th>Parcel ID</th>
+                        <th>Amount</th>
+                        <th>Transaction</th>
+                        <th>Paid At</th>
+                    </tr>
+                </thead>
+
+                <tbody>table body</tbody>
+            </table>
         </div>
     );
 };
 
-export default page;
+export default PaymentHistory;
